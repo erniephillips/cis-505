7ccb30373155e8cc5a79af4224a2cffff66e86be
Ernie Phillips <eraphillips@my365.bellevue.edu>
2021-08-28 21:21:40 -0400

    adding the hello world program for assignment 1.2

---
+++ Module_1/HelloWorldApp/PhillipsHelloWorld.java
@@ -0,0 +1,11 @@
+/*
+    Author: Ernest Phillips III
+    Date: 08/28/2021
+*/
+public class PhillipsHelloWorld{
+    //main method ran by compiler
+    public static void main(String args[]){
+        //output a string to the console
+        System.out.println("Hello world from Ernie Phillips.");
+    }
+}
