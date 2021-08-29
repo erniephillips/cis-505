385798f33ee297dc505fe4a922adab256fc1d8a6
Ernie Phillips <eraphillips@my365.bellevue.edu>
2021-08-28 21:26:36 -0400

    did not read instructions thouroughly. Applying fixes

--- Module_1/HelloWorldApp/PhillipsHelloWorld.java
+++ Module_1/HelloWorldApp/PhillipsHelloWorld.java
@@ -5,7 +5,10 @@
 public class PhillipsHelloWorld{
     //main method ran by compiler
     public static void main(String args[]){
+        //create instance vars for first and last name
+        String firstName = "Ernie", lastName = "Phillips";
+
         //output a string to the console
-        System.out.println("Hello world from Ernie Phillips.");
+        System.out.printf("Hello world from " + firstName + " " + lastName + ".");
     }
 }
