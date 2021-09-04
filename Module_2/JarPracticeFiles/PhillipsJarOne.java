a1049127427235908d81060a33b92585bcbb9926
Ernie Phillips <eraphillips@my365.bellevue.edu>
2021-09-04 19:24:35 -0400

    adding assignment 2 contents

---
+++ Module_2/JarPracticeFiles/PhillipsJarOne.java
@@ -0,0 +1,14 @@
+/*
+    Author: Ernest Phillips III
+    Date: 09/04/2021
+*/
+public class PhillipsHelloWorld{
+  //main method ran by compiler
+  public static void main(String args[]){
+      //create instance vars for first and last name
+      String firstName = "Ernie", lastName = "Phillips";
+
+      //output a string to the console
+      System.out.printf("Hello world from " + firstName + " " + lastName + ".");
+  }
+}
