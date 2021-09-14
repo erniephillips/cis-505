04a9c4a6924ae45182da860a11bba6120470f95c
Ernie Phillips <eraphillips@my365.bellevue.edu>
2021-09-13 21:36:20 -0400

    adding product file for module 4

---
+++ Module_4/BowlingShopApp/Product.java
@@ -0,0 +1,59 @@
+/*
+    Author: Ernest Phillips III
+    Date: 09/13/2021
+    Purpose: Product class of type Super that will be consumed by child classes
+*/
+public class Product {
+  private String code = "";
+  private String description = "";
+  private double price = 0;
+
+  public Product() { } //default constructor
+
+  public Product(String code, String description, double price) { //parameterized constructor
+    this.code = code;
+    this.description = description;
+    this.price = price;
+  }
+
+  //getter / setter methods
+  public String getCode() {
+    return this.code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+  public String getDescription() {
+    return this.description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public double getPrice() {
+    return this.price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public Product code(String code) {
+    setCode(code);
+    return this;
+  }
+ 
+
+  @Override
+  public String toString() {
+    return "{" +
+      " code='" + getCode() + "'\n" +
+      ", description='" + getDescription() + "'\n" +
+      ", price='" + getPrice() + "'" +
+      "}";
+  }
+  
+}
